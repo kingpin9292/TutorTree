cda0e55d8c364554242487f0b3b5fb96ccc0fbf0
kingpin9292 <jakarea9292@gmail.com>
2026-04-05 21:59:29 -0400

    implement Navigation Bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const Page = () => {
   return (
     <div>
       <h1 className="text-2xl underline">Welcome to my SaaS app</h1>
+      <Button className="bg-purple-500 hover:bg-yellow-600 text-yellow-400">Lets get started</Button>
     </div>
   );
 };
