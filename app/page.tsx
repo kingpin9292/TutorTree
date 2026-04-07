c41cf72ed0582b9eb04e3acf283736064a56e5a2
kingpin9292 <jakarea9292@gmail.com>
2026-04-06 22:43:18 -0400

    implement HomePage part-1

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,29 @@
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
 import { Button } from "@/components/ui/button";
+import { recentSessions } from "@/constants";
 import React from "react";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to my SaaS app</h1>
-      <Button className="bg-purple-500 hover:bg-yellow-600 text-yellow-400">Lets get started</Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+
+      <section className="home-section">
+        <CompanionCard id="1" name="Companion 1" topic="Topic 1" subject="Subject 1" duration={30} color="#f2c94c" />
+        <CompanionCard id="1" name="Companion 1" topic="Topic 1" subject="Subject 1" duration={30} color="#f87171" />
+        <CompanionCard id="1" name="Companion 1" topic="Topic 1" subject="Subject 1" duration={30} color="#f39c12" />
+      </section>
+
+      <section className="home-section">
+        <CompanionsList
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        {/* <CTA /> */}
+      </section>
+    </main>
   );
 };
 
